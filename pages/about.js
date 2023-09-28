f902bd3882c13c95e78ae79d64e9a857cb0a8559
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-09-28 10:43:35 +0100

    Installed and configured Prettier

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 export default function About() {
-    return (
-        <main>
-            <h1>Hello There</h1>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Hello There</h1>
+    </main>
+  );
 }
