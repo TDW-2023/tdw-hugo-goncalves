59bb1eb7991bef253cd4d6eae0536b02570cc927
Hugo Gonçalves <hugomiguel_2001_@sapo.pt>
2023-10-12 11:20:00 +0100

    Added Next.js build export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        unoptimized: true
+    },
+    output: "export",
+    experimental: {
+        forceSwcTransforms: true,
+    },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
